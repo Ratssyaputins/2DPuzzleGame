0db57d0b7571b6070d5f1e60445299a041ffe555
mocavaMars33 <hgkemail@gmail.com>
2021-03-28 12:51:12 +1100

    Update

--- Assets/Scripts/SudokuGrid.cs
+++ Assets/Scripts/SudokuGrid.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class SudokuGrid : MonoBehaviour
 {
+    public int columns = 0;
+    public int rows = 0;
+    public float square_offset = 0.0f;
+    public GameObject grid_square;  //"our prefab"
+    public Vector2 start_position = new Vector2(0.0f, 0.0f);
+    public float square_scale = 1.0f;
+
+    private List<GameObject> grid_squares = new List<GameObject>();
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (grid_square.GetComponent<GridSquare>() == null)
+            Debug.LogError("This Game Object needs to have Gr "); //"this check id our prefab has gridsquare script attahced"
     }
 
     // Update is called once per frame
